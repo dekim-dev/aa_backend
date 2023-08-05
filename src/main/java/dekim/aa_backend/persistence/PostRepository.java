c3575f0107314632055c0dccdfa5df61b20de67a
dekim-dev <dekimdev@gmail.com>
2023-08-05 23:59:41 +0900

    add repositories

---
+++ src/main/java/dekim/aa_backend/persistence/PostRepository.java
@@ -0,0 +1,9 @@
+package dekim.aa_backend.persistence;
+
+import dekim.aa_backend.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
