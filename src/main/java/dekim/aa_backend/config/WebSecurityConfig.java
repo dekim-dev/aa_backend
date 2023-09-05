2d6fc1fcced1bd1238d0d99ce9658787b1dbd0ba
dekim-dev <dekimdev@gmail.com>
2023-09-05 19:09:02 +0900

    :wrench: Update Jwt and Web Security Config files
    due to Spring Security and JWT changes

--- src/main/java/dekim/aa_backend/config/WebSecurityConfig.java
+++ src/main/java/dekim/aa_backend/config/WebSecurityConfig.java
@@ -1,34 +1,25 @@
 package dekim.aa_backend.config;
 
-import dekim.aa_backend.config.jwt.TokenAuthenticationFilter;
-import dekim.aa_backend.service.UserDetailService;
+import dekim.aa_backend.config.jwt.JwtAccessDeniedHandler;
+import dekim.aa_backend.config.jwt.JwtAuthenticationEntryPoint;
+import dekim.aa_backend.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.CorsFilter;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @RequiredArgsConstructor
 @Configuration // 설정파일로 등록
-@EnableWebSecurity
 public class WebSecurityConfig {
 
-  private final UserDetailService userService;
-  private final TokenAuthenticationFilter tokenAuthenticationFilter;
-
-//  @Bean
-//  public WebSecurityCustomizer configure() { // SS기능 비활성화
-//    return (web) -> web.ignoring()
-//            .requestMatchers(toH2Console());
-//  }
+  private final TokenProvider tokenProvider;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -38,28 +29,20 @@ public class WebSecurityConfig {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // OPTIONS 요청 허용
-//                    .requestMatchers("/signin", "/signup", "/clinics/**", "/todo-item").permitAll()
-//                    .anyRequest().authenticated()
+                    .requestMatchers(HttpMethod.OPTIONS, "/auth/**").permitAll() // OPTIONS 요청 허용
                             .anyRequest().permitAll()
             )
+            .exceptionHandling(customizer -> customizer
+                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                    .accessDeniedHandler(jwtAccessDeniedHandler)
+            )
             .formLogin(formLogin -> formLogin.disable())
-            .addFilterBefore(tokenAuthenticationFilter, CorsFilter.class);
+            .apply(new JwtSecurityConfig(tokenProvider));
     return http.build();
   }
 
-//  @Bean
-//  public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
-//    return http
-//            .getSharedObject(AuthenticationManagerBuilder.class)
-//            .userDetailsService(userService)
-//            .passwordEncoder(bCryptPasswordEncoder)
-//            .and()
-//            .build();
-//  }
-
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
